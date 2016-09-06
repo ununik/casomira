1d9fe7fcc06b9fbb44bf3bfb2fea80a656c9c8f2
Â= <=>
2016-09-06 07:39:10 +0200

    added img

--- ajax/main_menu.php
+++ ajax/main_menu.php
@@ -1,9 +1,11 @@
 <?php
 include __DIR__ . '/../autoload.php';
 
-$container = '<div onclick="page(\'kategorie\')">Kategorie</div>';
-$container .= '<div onclick="page(\'startovni_listina\')">Startovní listina</div>';
-$container .= '<div onclick="page(\'mereni_mezicasu\')">Měření mezičasů</div>';
-$container .= '<div onclick="page(\'vysledky\')">Výsledky</div>';
+$container = '<div class="menu">';
+$container .= '<div onclick="page(\'kategorie\')" class="startovniListina menuButton"></div>';
+$container .= '<div onclick="page(\'startovni_listina\')" class="startovniListina menuButton"></div>';
+$container .= '<div onclick="page(\'mereni_mezicasu\')" class="startovniListina menuButton"></div>';
+$container .= '<div onclick="page(\'vysledky\')" class="startovniListina menuButton"></div>';
+$container .= '</div>';
 
 print $container;
